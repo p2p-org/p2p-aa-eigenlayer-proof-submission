076159b638c3a6b2456b61ae8579c3eda4b22bed
Alexander Biryukov <alexander.biryukov@p2p.org>
2024-09-12 00:44:51 +0400

    deploy

---
+++ script/Deploy.s.sol
@@ -0,0 +1,24 @@
+// SPDX-FileCopyrightText: 2024 P2P Validator <info@p2p.org>
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.17;
+
+import {Script} from "forge-std/Script.sol";
+import "../src/proofSubmitterFactory/ProofSubmitterFactory.sol";
+import "../src/proofSubmitter/ProofSubmitter.sol";
+
+contract Deploy is Script {
+    function run()
+        external
+        returns (ProofSubmitterFactory factory, ProofSubmitter proofSubmitter)
+    {
+        uint256 deployerKey = vm.envUint("PRIVATE_KEY");
+
+        vm.startBroadcast(deployerKey);
+        factory = new ProofSubmitterFactory();
+        proofSubmitter = factory.createProofSubmitter{value: 0.01 ether}();
+        vm.stopBroadcast();
+
+        return (factory, proofSubmitter);
+    }
+}
